f3ff0f2692816ff3d87534ee9563b80037aee880
Richard Rubalcava <rubalcava@outlook.com>
2016-10-08 13:37:41 -0700

    feat: All unit tests pass

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -7,38 +7,71 @@ import psycopg2
 
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
+    """Connect to the PostgreSQL database.
+    Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("DELETE FROM matches")
+    c.execute("UPDATE players SET wins = 0")
+    c.execute("UPDATE players SET matches = 0")
+
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("DELETE FROM players")
+
+    DB.commit()
+    DB.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT count(id) AS num FROM players")
+
+    my_number = c.fetchall()[0][0]
+
+    DB.commit()
+    DB.close()
+
+    return my_number
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
+
+    The database assigns a unique serial id number for the player.
+    (This should be handled by your SQL database schema, not in your
+    Python code.)
+
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("INSERT INTO players(name, wins, matches) VALUES(%s, 0, 0)" , (name,))
+
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
+    """Returns a list of the players and their win records,
+    sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first
+    place, or a player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -47,6 +80,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT * from players ORDER BY wins DESC")
+    my_tuple = c.fetchall()
+
+    DB.commit()
+    DB.close()
+
+    return my_tuple
 
 
 def reportMatch(winner, loser):
@@ -56,16 +98,25 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("INSERT INTO matches values(%s,%s)",  (winner, loser))
+    c.execute("UPDATE players SET wins = (wins + 1) WHERE id = %s", (winner,))
+    c.execute("UPDATE players SET matches = (matches + 1) WHERE id = %s", (winner,))
+    c.execute("UPDATE players SET matches = (matches + 1) WHERE id = %s", (loser,))
+
+    DB.commit()
+    DB.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
+    player with an equal or nearly-equal win record, that is, a player
+    adjacent to him or her in the standings.
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +124,22 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    DB = psycopg2.connect("dbname=tournament")
+    c = DB.cursor()
+    c.execute("SELECT id, name FROM players ORDER BY wins DESC")
+    sorted_winners = c.fetchall()
 
+    DB.commit()
+    DB.close()
 
+    list_of_pairs = []
+
+    if len(sorted_winners) % 2 == 0:
+        for i in range(0, len(sorted_winners), 2):
+            match_pair = (sorted_winners[i][0], sorted_winners[i][1],
+                          sorted_winners[i+1][0], sorted_winners[i+1][1])
+            list_of_pairs.append(match_pair)
+
+        return list_of_pairs
+    else:
+        print("You don't have an even number of players!")
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- If there's already a database, delete it
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+-- Load the database
+\c tournament;
+
+CREATE TABLE players(id serial primary key, name text, wins integer, matches integer);
+
+CREATE TABLE matches(winner integer, loser integer);
